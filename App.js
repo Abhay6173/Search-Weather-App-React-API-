8102b642870412aabab5e54383fb88efb30742d1
Abhay6173 <89240753+Abhay6173@users.noreply.github.com>
2022-12-27 00:32:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import Temprature from "./Components/Temprature";
+import './App.css';
+
+function App() {
+  return (
+    < Temprature/>
+  );
+}
+
+export default App;
